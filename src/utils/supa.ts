357faa3b1afc0af5dcc90c1e55174a0ca8ebb8cb
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-10-16 11:33:17 -0400

    feat: create utils supabase crud

--- src/utils/supa.ts
+++ src/utils/supa.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@nestjs/common'
 import { createClient } from '@supabase/supabase-js'
 @Injectable()
-export class SupabaseUtils{
-constructor(
-    supabaseUrl: string = process.env.SUPABASE_URL,
-    supabaseKey: string = process.env.SUPABASE_KEY
-){
+export class SupabaseUtils {
+    supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+    constructor() {}
 
-    
+    select() {
+        this.supabase.from('massage').select().eq('id', 3)
+        return 'Estou buscando dados!!'
+    }
+    insert() {
+        this.supabase.from('massage').insert({ massage: 'Olá Mundo' })
+        return 'Estou inserindo dados!!'
+    }
+    update() {
+        this.supabase.from('massage').update({massage: "Primeira mensagem que foi atualizada"}).eq('id', 1)
+        return 'Estou atualizando dados!!'
+    }
+    deleteAll() {
+        this.supabase.from('massage').delete()
+        return 'Estou deletando todos dados!!'
+    }
 }
-
-}
-
